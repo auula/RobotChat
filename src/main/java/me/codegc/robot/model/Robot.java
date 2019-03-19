b6bb491066c935e5d1f2533270dbfc0c9bab6ffd
JDode <2420498526@qq.com>
2019-03-19 10:20:57 +0800

    👬优化导包

--- src/main/java/me/codegc/robot/model/Robot.java
+++ src/main/java/me/codegc/robot/model/Robot.java
@@ -6,12 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import utils.HttpRequestClient;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.Map;
 
 /**
